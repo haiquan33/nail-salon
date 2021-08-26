6fcd46785cc3cbea7a0880bdac6650cdd49a3fa9
pnqanh <quocanh.inbox@gmail.com>
2021-08-27 00:03:41 +0700

    add-booking: feat: add booking, gift-card link

--- components/booking/components/Button.tsx
+++ components/booking/components/Button.tsx
@@ -3,6 +3,7 @@ import { BookingIcon, CheckinIcon } from '.';
 export const Button = ({ href, type }: { href: string, type: 'Booking' | 'Checkin' }) => {
     return <a
         href={ href }
+        target="_blank"
         className="flex items-center px-3 py-1 mx-3 sm:mx-3 relative text-xs text-red-600 border-2 border-red-600 rounded-2xl bg-white"
     >
         { type === 'Booking' ? <BookingIcon/> : <CheckinIcon/> }
